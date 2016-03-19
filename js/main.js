f7bf8e4b543ecf141d4eee8356223b89b95d4d97
emilycopp <emily.copp@bell.net>
2016-03-19 10:45:41 -0400

    created form for simple to do list

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var $input = $('#to-do');
+var $list = $('.list');
+
+$('form').on('submit', function (e) {
+  var $li = $('<li>');
+
+  e.preventDefault();
+  $li.html($input.val());
+  $list.append($li);
+  $input.val('');
+});
+
